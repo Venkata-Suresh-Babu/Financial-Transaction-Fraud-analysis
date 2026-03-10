d8b2c9fe81ac73d8b1d035b68f304807a2d8b7c4
Venkata-Suresh-Babu <sureshbabu.pesupati2000@gmail.com>
2026-03-10 12:37:58 +0530

    Add fraud detection analysis notebook, python script, and requirements

---
+++ fraud_detection.py
@@ -0,0 +1,40 @@
+import joblib
+import streamlit as st
+import pandas as pd
+import joblib
+
+model = joblib.load('fraud_detection_pipeline.pkl')
+
+st.title('Fraud Detection Prediction app')
+st.markdown('please enter the transaction details and click on the predict button')
+st.divider()
+
+transaction_type = st.selectbox('Transaction Type', ['PAYMENT', 'TRANSFER', 'CASH_OUT', 'DEPOSIT'])
+amount = st.number_input('amount', min_value=0.0, value=1000.0)
+oldbalanceOrg = st.number_input('Old Balance (Sender)', min_value=0.0, value=10000.0)
+newbalanceOrig = st.number_input('New Balance (Sender)', min_value= 0.0, value=9000.0)
+oldbalanceDest = st.number_input('Old Balance (Receiver)', min_value=0.0, value=0.0)
+newbalanceDest = st.number_input('New Balance (Receiver)', min_value=0.0, value=0.0)
+
+#'step', 'type', 'amount', 'nameOrig', 'oldbalanceOrg', 'newbalanceOrig',
+# 'nameDest', 'oldbalanceDest', 'newbalanceDest', 'isFraud','isFlaggedFraud', 
+# 'BalanceDiffOrginal', 'BalanceDiffDest', 'oldbalanceOrg'
+
+if st.button('Predict'):
+    input_data = pd.DataFrame([{
+        'type' : transaction_type,
+        'amount' : amount,
+        'oldbalanceOrg' : oldbalanceOrg,
+        'newbalanceOrig' : newbalanceOrig,
+        'oldbalanceDest' : oldbalanceDest,
+        'newbalanceDest' : newbalanceDest
+    }])
+
+    prediction = model.predict(input_data)
+
+    st.subheader(f'Prediction value: {int(prediction[0])}')
+
+    if prediction[0] == 1:
+        st.error('This transaction looks like fraud')
+    else:
+        st.success('This transaction is not fraud')
